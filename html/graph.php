1657a94a3347c1215c872dd62512e3186121936a
Matt Haver <matthaver@gmail.com>
2016-10-02 21:41:31 -0400

    test

---
+++ html/graph.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+echo "<CENTER><font color='white': font size= '7'> <u>ready set go:</u></font></CENTER>";
+
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('/home/rhubarb0122/projects/Ultrasonic.db');
+    }
+}
+
+echo "<CENTER><font color='white': font size= '7'> <u>right befor db def</u></font></CENTER>";
+$db = new MyDB();
+
+//$db->exec('CREATE TABLE foo (bar STRING)');
+//$db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
+
+$result = $db->query('SELECT * FROM SumpLog');
+var_dump($result->fetchArray());
+echo "<CENTER><font color='white': font size= '7'> <u>$result</u></font></CENTER>";
+
+//$db = sqlite_open('/Home/rhubarb0122/projects/Ultrasonic.db', 0666, $error);
+//echo "<CENTER><font color='white': font size= '7'> <u>$db</u></font></CENTER>";
+//$query = "SELECT * FROM SumpLog WHERE Waterlevel = '3.3'";
+//$result = sqlite_query($db, $query);
+//if (!$result) die("NOPE");
+//echo "<CENTER><font color='white': font size= '7'> <u>$query</u></font></CENTER>";
+
+ 
+?>
---
+++ html/index.php
@@ -0,0 +1,58 @@
+﻿﻿<!DOCTYPE html>
+<!--TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/ -->
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Raspberry Pi Gpio</title>
+        
+       /* <style type="text/css">
+        .myImage {
+			margin: auto;
+			display: block;
+		}
+        
+        </style>*/
+        
+    </head>
+ 
+    <body style="background-color: black;">
+    <!-- On/Off button's picture -->
+	<?php
+	
+	include 'test.php';
+	
+	$val_array = array(0,0,0,0,0,0,0,0);
+
+		system("gpio mode 25 out");
+		exec ("gpio read 25", $val_array[0], $return );
+		exec ("gpio read 25", $val_array[2], $return );
+		system("gpio mode 7 out");
+		exec ("gpio read 7", $val_array[1], $return );
+		
+	//for loop to read the value
+	$i =0;
+	for ($i = 0; $i < 3; $i++) {
+		//if off
+		if ($val_array[$i][0] == 0 ) {
+			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' class='myImage' onclick='change_pin (".$i.");'/>");
+		}
+		//if on
+		if ($val_array[$i][0] == 1 ) {
+			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' class='myImage' onclick='change_pin (".$i.");'/>");
+		}	 
+	}
+	echo "<br><br>";
+	echo "<br>";
+	echo "<CENTER><font color='white': font size= '7'> <u>SUMP PUMP STATUS:</u></font></CENTER>";
+	echo "<br>";
+	echo "<CENTER><font color='white': font size= '6'> ".tailCustom()."</font></CENTER>";
+
+include 'graph.php';
+
+	?>
+	 
+	<!-- javascript -->
+	<script src="script.js"></script>
+    </body>
+</html>
